bf8fda364e23b552566de66250d51445bfbc50c1
jeandenassau <denassaujdjean@gmail.com>
2025-03-23 17:00:22 -0400

    Fixed tokens for section and tutor services

--- src/main/java/com/app/Section/SectionController.java
+++ src/main/java/com/app/Section/SectionController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.*;
 import com.app.Dto.*;
 import com.app.security.JWTGenerator;
 
+import jakarta.servlet.http.HttpServletRequest;
+
+import com.app.security.JWTAuthenticationFilter;
+
+
 import java.util.List;
 
 @RestController
@@ -17,42 +22,47 @@ public class SectionController {
     // JWT Generator
     JWTGenerator jwt;
 
-    public SectionController(SectionService sectionService) {
+    public SectionController(SectionService sectionService, JWTGenerator jwt) {
         this.sectionService = sectionService;
-        this.jwt = new JWTGenerator();
+        this.jwt = jwt;
     }
 
     // get request to get all sections for that user
     @GetMapping
-    public List<SectionEntity> getAllSections(@RequestHeader("Authorization") String token) {
+    public List<SectionEntity> getAllSections(HttpServletRequest request) {
+        String token = JWTAuthenticationFilter.getJWTFromRequest(request);
         String userID = jwt.getUserIdFromJWT(token);
         return sectionService.getAllSections(userID);
     }
 
     // get request to get a section by id
     @GetMapping("/{id}")
-    public SectionEntity getSectionById(@RequestHeader("Authorization") String token, @PathVariable String id) {
+    public SectionEntity getSectionById(HttpServletRequest request, @PathVariable String id) {
+        String token = JWTAuthenticationFilter.getJWTFromRequest(request);
         String userID = jwt.getUserIdFromJWT(token);
         return sectionService.getSectionById(userID, id);
     }
 
     // post request to add a section
     @PostMapping
-    public ResponseEntity<SectionEntity> addSection(@RequestHeader("Authorization") String token, @RequestBody SectionDto sectionDto) {
+    public ResponseEntity<SectionEntity> addSection(HttpServletRequest request, @RequestBody SectionDto sectionDto) {
+        String token = JWTAuthenticationFilter.getJWTFromRequest(request);
         String userID = jwt.getUserIdFromJWT(token);
         return new ResponseEntity<>(sectionService.addSection(userID, sectionDto), HttpStatus.CREATED);
     }
 
     // put request to update a section
     @PatchMapping
-    public ResponseEntity<SectionEntity> updateSection(@RequestHeader("Authorization") String token, @RequestBody SectionDto sectionDto) {
+    public ResponseEntity<SectionEntity> updateSection(HttpServletRequest request, @RequestBody SectionDto sectionDto) {
+        String token = JWTAuthenticationFilter.getJWTFromRequest(request);
         String userID = jwt.getUserIdFromJWT(token);
         return new ResponseEntity<>(sectionService.updateSection(userID, sectionDto), HttpStatus.OK);
     }
 
     // delete request to delete a section
     @DeleteMapping("/{id}")
-    public void deleteSection(@RequestHeader("Authorization") String token, @PathVariable String id) {
+    public void deleteSection(HttpServletRequest request, @PathVariable String id) {
+        String token = JWTAuthenticationFilter.getJWTFromRequest(request);
         String userID = jwt.getUserIdFromJWT(token);
         sectionService.deleteSection(userID, id);
     }
--- src/main/java/com/app/Tutor/TutorController.java
+++ src/main/java/com/app/Tutor/TutorController.java
@@ -5,7 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.app.Dto.*;
+import com.app.security.JWTAuthenticationFilter;
 import com.app.security.JWTGenerator;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.List;
 
@@ -15,9 +18,9 @@ public class TutorController {
     private final TutorService tutorService;
     JWTGenerator jwt;
 
-    public TutorController(TutorService tutorService) {
+    public TutorController(TutorService tutorService, JWTGenerator jwt) {
         this.tutorService = tutorService;
-        this.jwt = new JWTGenerator();
+        this.jwt = jwt;
     }
 
     // get request to get all tutors
@@ -34,21 +37,24 @@ public class TutorController {
 
     // post request to add a tutor
     @PostMapping
-    public ResponseEntity<TutorEntity> addTutor(@RequestHeader("Authorization") String token, @RequestBody TutorDto tutorDto) {
+    public ResponseEntity<TutorEntity> addTutor(HttpServletRequest request, @RequestBody TutorDto tutorDto) {
+        String token = JWTAuthenticationFilter.getJWTFromRequest(request);
         String userID = jwt.getUserIdFromJWT(token);
         return new ResponseEntity<>(tutorService.addTutor(userID, tutorDto), HttpStatus.CREATED);
     }
 
     // put request to update a tutor
     @PatchMapping
-    public ResponseEntity<TutorEntity> updateTutor(@RequestHeader("Authorization") String token, @RequestBody TutorDto tutorDto) {
+    public ResponseEntity<TutorEntity> updateTutor(HttpServletRequest request, @RequestBody TutorDto tutorDto) {
+        String token = JWTAuthenticationFilter.getJWTFromRequest(request);
         String userID = jwt.getUserIdFromJWT(token);
         return new ResponseEntity<>(tutorService.updateTutor(userID, tutorDto), HttpStatus.OK);
     }
 
     // delete request to delete a tutor based on the userID passed in the token
     @DeleteMapping()
-    public void deleteTutor(@RequestHeader("Authorization") String token) {
+    public void deleteTutor(HttpServletRequest request) {
+        String token = JWTAuthenticationFilter.getJWTFromRequest(request);
         String userID = jwt.getUserIdFromJWT(token);
         tutorService.deleteTutor(userID);
     }
